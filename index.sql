0a73fd37b56f370c39885f52066781392780fbda
realsticklabs <sinkfieldalex8@gmail.com>
2024-09-14 09:13:32 -0400

    Create index.sql

---
+++ index.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE forum_chat;
+USE forum_chat;
+
+CREATE TABLE messages (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(50) NOT NULL,
+  message TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
